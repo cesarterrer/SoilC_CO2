53b007de421cd73621d9d490fa19bbf467e36b03
Terrer-Moreno <terrermoreno1@seatac14.llnl.gov>
2019-12-17 18:09:25 -0800

    Upscaled using depth = 20cm

--- Plot_maps.R
+++ Plot_maps.R
@@ -1,11 +1,6 @@
 library(devtools)
-dev_mode(TRUE)
-install_github("hadley/scales", force=TRUE)
-# main branch of development
 install_github("hadley/ggplot2")
 # (Optional) install_github("ggplot2", "kohske", "feature/new-guides-with-gtable")
-# Exit dev mode
-dev_mode(FALSE)
 library(ggplot2)
 library(raster)
 library(RColorBrewer)
@@ -146,33 +141,4 @@ scale_fill_gradientn(colours = cpt(pal = "arendal_temperature", rev=T, n=7),
 
 save_plot("graphs/CO2absEffect.pdf", p, base_aspect_ratio = 1.5, dpi=300)
 save_plot("graphs/CO2absEffect.png", p, dpi= 1200,type = "cairo-png",base_aspect_ratio = 1.5)
-
-
-###################
-### ZONAL STATS ###
-###################
-esa <- raster("~/OneDrive/OneDrive - Universitat Autònoma de Barcelona/IIASA/maps/ESA_2012_aggregated0p25.tif")
-legend <- read.csv("ESA_classes.csv")
-pix <- raster("maps/CO2abslEffect_RF_pixel.tif")  # pixel
-gm2 <- raster("maps/CO2abslEffect_RF_gm2.tif")
-soc <- raster("~/OneDrive/OneDrive - Universitat Autònoma de Barcelona/IIASA/maps/SOC_015cm_aggregated0p25_gm2.tif") # g/m2
-soc <- mask(soc, pix)
-soctha <- soc*0.01 # t(Mg)/ha
-soc_a <- area(soctha) # get area of projected raster.
-soc_pixel <- soctha * soc_a * 100 # area is in km2 multiply by 100 to get ha 
-
-s <- stack(esa,pix,gm2,soc_pixel)
-s.df <- as.data.frame(s)
-names(s.df) <- c("ESA", "Mg", "gm2","SOCMg")
-s.df <- left_join(s.df,legend, by=c("ESA" = "NB_LAB"))
-library(magrittr)
-biome <- s.df %>% dplyr::filter(!is.na(Mg), Mg != 0) %>% group_by(ESAagg) %>%  
-  dplyr::summarise (MgSum= sum(Mg), Cstock=sum(SOCMg)) %>% 
-  dplyr::mutate(PgSum=round(MgSum*10^-9, digits=2), Perc=round((MgSum*100)/Cstock, digits=1)) %>%
-  mutate(ESAagg=as.character(ESAagg)) %>% ungroup() %>%
-  bind_rows(summarise(.,ESAagg = "Total", MgSum = sum(MgSum), Cstock=sum(Cstock),PgSum = sum(PgSum))%>% 
-              mutate(Perc=round((MgSum*100)/Cstock, digits=1)))
-
-write.csv(biome, file = "Summary_ESA.csv")
-
 
--- Upscaling_MF.R
+++ Upscaling_MF.R
@@ -14,8 +14,8 @@ dat <- filter(dat, nyears >= 0.5)
 dat$myc <- with(dat, ifelse(Myc=="ECM", 100,0))
 
 #################### META FOREST #################
-X.abs <- dat[, c("abs","abs.var", "N", "amb", "biomass","myc")] %>% 
-  rename(yi=abs, vi=abs.var, "Nitrogen.fertilization"=N, "Effect.biomass"=biomass, "Cstock" = amb)
+X.abs <- dat[, c("abs","abs.var", "N", "amb", "biomass","myc","Depth..cm.")] %>% 
+  rename(yi=abs, vi=abs.var, "Nitrogen.fertilization"=N, "Effect.biomass"=biomass, "Cstock" = amb, "Soil.depth"=Depth..cm.)
 #Set random seed
 set.seed(4)
 forest.abs <- MetaForest(yi ~ ., 
@@ -29,7 +29,7 @@ forest.inbag <- MetaForest(yi ~ .,
                          num.trees = 5000)
 forest.abs$forest$r.squared
 plot(forest.abs)
-
+VarImpPlot(forest.abs)
 # Raster layers
 biomass <- raster("~/OneDrive/OneDrive - Universitat Autònoma de Barcelona/IIASA/Upscaling_Biomass/Maps/CO2relEffect.tif")
 biomass[is.na(biomass)] <- 0
@@ -46,7 +46,7 @@ myc <- resample(myc,biomass,method="bilinear")
 s<- stack(biomass, soc, fert, myc)
 s.df <- as.data.frame(s,xy=TRUE) %>% rename("Nitrogen.fertilization"=ESA_Nfertilization_category, "Effect.biomass"=layer.1, 
                                             "Cstock" = SOC_015cm_aggregated0p25_gm2, "myc" = layer.2) %>%
-  mutate(Nitrogen.fertilization=factor(s.df$Nitrogen.fertilization))
+  mutate(Nitrogen.fertilization=factor(Nitrogen.fertilization), Soil.depth=20)
 
 #test<- data.frame(Effect.biomass=c(01,0,0.25), Cstock=c(1000,5000,10000), Nitrogen.fertilization=c("Nhigh","Nhigh","low"))
 #test<- data.frame(Effect.biomass=c(rep(01,10),rep(0,10),rep(0.25,10)), Cstock=rep(1000,30), Nitrogen.fertilization=rep("Nhigh",30))
@@ -69,7 +69,7 @@ absEStha <- absES*0.01 # Mg/ha == tonnes/ha. I need to compute the total Mg in t
 writeRaster(absEStha,"maps/CO2abslEffect_RF_tha.tif",format="GTiff",overwrite=TRUE)
 a <- area(absEStha) # get area of projected raster.
 absESpixel <- absEStha * a * 100 # area is in km2 multiply by 100 to get ha 
-cellStats(absESpixel,"sum", na.rm=T) * 10^(-9)   # Pg soil C at 0-15cm
+cellStats(absESpixel,"sum", na.rm=T) * 10^(-9)   # Pg soil C at 0-30cm
 writeRaster(absESpixel,"maps/CO2abslEffect_RF_pixel.tif",format="GTiff",overwrite=TRUE)
 
 
